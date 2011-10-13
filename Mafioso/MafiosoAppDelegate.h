0649f7a0c2eebd50dd93cfe64b69b06b434344af
Pedro Góes <pedro@imac-de-pedro.(none)>
2011-10-12 22:15:50 -0300

    Initial Commit

---
+++ Mafioso/MafiosoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  MafiosoAppDelegate.h
+//  Mafioso
+//
+//  Created by Pedro Góes on 12/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MafiosoAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ Mafioso/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Mafioso
+//
+//  Created by Pedro Góes on 12/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
